1d1a4c9b69463e3d756c50a4fa6b369ac801011d
anijalohidakshan96 <anija111lohidakshan@gmail.com>
2018-03-07 15:06:22 +0530

    committed

---
+++ table.py
@@ -0,0 +1,6 @@
+def table(n):
+        print('{0,3} {1:6} {2:9} {3:12}'.format('i','i**2','i**3','i**4'))
+        for i in range(0,n):
+                print('{0,3} {1:6} {2:9} {3:12}'.format(i,i**2,i**3,i**4))
+               
+	
